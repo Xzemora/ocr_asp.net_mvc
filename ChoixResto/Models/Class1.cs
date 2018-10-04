2ce80bdbba2b85f8db43829bcd76faa302c7985d
Xzemora <alexandre.reynaud@custom-dev.fr>
2018-10-04 18:01:04 +0200

    tp test 17/19

--- ChoixResto/Models/Class1.cs
+++ ChoixResto/Models/Class1.cs
@@ -12,6 +12,8 @@ namespace ChoixResto.Models
         public int Id { get; set; }
         [Required]
         public string Prenom { get; set; }
+        [Required]
+        public string Mdp { get; set; }
     }
 
     [Table("Restos")]
@@ -32,15 +34,28 @@ namespace ChoixResto.Models
 
     public class Sondage
     {
+        public Sondage()
+        {
+            Date = DateTime.Now;
+            Votes = new List<Vote>();
+        }
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public virtual List<Vote> Votes { get; set; }
     }
-    
+
+    public class Resultats
+    {
+        public string Nom { get; set; }
+        public string Telephone { get; set; }
+        public int NombreDeVotes { get; set; }
+    }
+
     public class BddContext : DbContext
     {
         public DbSet<Sondage> Sondages { get; set; }
         public DbSet<Resto> Restos { get; set; }
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
     }
 
     public class Dal : IDal
@@ -55,6 +70,20 @@ namespace ChoixResto.Models
         public List<Resto> ObtientTousLesRestaurants()
         {
             return bdd.Restos.ToList();
+        }
+
+        public Resto ObtientResto(int idResto)
+        {
+            List<Resto> restos = ObtientTousLesRestaurants();
+            
+            for (int j = 0; j < restos.Count; j++)
+            {
+                if (restos[j].Id == idResto)
+                {
+                    return restos[j];
+                }
+            }
+            return null;
         }
 
         public void Dispose()
@@ -77,6 +106,133 @@ namespace ChoixResto.Models
                 restoTrouve.Telephone = telephone;
                 bdd.SaveChanges();
             }
+        }
+
+        public bool RestaurantExiste(string nom)
+        {
+            List<Resto> restos = ObtientTousLesRestaurants();
+            for(int i=0; i<restos.Count; i++)
+            {
+
+                if(restos[i].Nom == nom)
+                {
+                    return true;
+                }                
+            }
+            return false;
+        }
+
+        
+        public Utilisateur ObtenirUtilisateur(int id)
+        {
+            List<Utilisateur> utilisateurs = bdd.Utilisateurs.ToList();
+            for(int i=0; i<utilisateurs.Count; i++)
+            {
+                if(utilisateurs[i].Id == id)
+                {
+                    return utilisateurs[i];
+                }                
+            }
+            return null;
+        }
+        public Utilisateur ObtenirUtilisateur(string id)
+        {
+            if (Int32.TryParse(id, out int intId))
+            {
+                return ObtenirUtilisateur(intId);
+            }
+            return null;           
+            
+        }
+
+        public int AjouterUtilisateur(string nom, string mdp)
+        {
+            bdd.Utilisateurs.Add(new Utilisateur { Prenom = nom, Mdp = mdp });
+            bdd.SaveChanges();
+            int idUtilisateur = bdd.Utilisateurs.Count();
+            return idUtilisateur;
+        }
+
+        public Utilisateur Authentifier(string prenom, string mdp)
+        {
+            List<Utilisateur> utilisateurs = bdd.Utilisateurs.ToList();
+            for (int i = 0; i < utilisateurs.Count; i++)
+            {
+                if (utilisateurs[i].Prenom == prenom && utilisateurs[i].Mdp == mdp)
+                {
+                    return utilisateurs[i];
+                }
+            }
+            return null;
+        }
+
+        public bool ADejaVote(int idSondage, string idUtilisateur)
+        {
+            if (ObtientSondage(idSondage) != null)
+            {
+                List<Vote> votes = ObtientSondage(idSondage).Votes;
+                for (int j = 0; j < votes.Count; j++)
+                {
+                    if (votes[j].Utilisateur == ObtenirUtilisateur(idUtilisateur) && votes[j].Utilisateur != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public int CreerUnSondage()
+        {
+            bdd.Sondages.Add(new Sondage());
+            bdd.SaveChanges();
+            int idSondage = bdd.Sondages.Count();
+            return idSondage;
+        }
+
+        public Sondage ObtientSondage(int idSondage)
+        {
+            List<Sondage> sondages = bdd.Sondages.ToList();
+            for (int i = 0; i < sondages.Count; i++)
+            {
+                if (sondages[i].Id == idSondage)
+                {
+                    return sondages[i];
+                }
+            }
+            return null;
+        }
+
+        public void AjouterVote(int idSondage, int idResto, int idUtilisateur)
+        {
+            
+            ObtientSondage(idSondage).Votes.Add(new Vote { Resto = ObtientResto(idResto), Utilisateur = ObtenirUtilisateur(idUtilisateur) });
+                
+            
+        }
+
+        public List<Resultats> ObtenirLesResultats(int idSondage)
+        {
+            List<Resultats> resultats = new List<Resultats>();
+            List<Resto> restos = ObtientTousLesRestaurants();
+            Sondage sondage = ObtientSondage(idSondage);
+            List<Vote> votes = sondage.Votes;
+            for (int i=0; i<restos.Count; i++)
+            {
+                Resultats resultat = new Resultats { Nom = restos[i].Nom, Telephone = restos[i].Telephone };
+                int count = 0;
+                for(int j =0;j<votes.Count; j++)
+                {
+                    if(votes[j].Resto.Nom == resultat.Nom)
+                    {
+                        count++;
+                    }
+                }
+                resultat.NombreDeVotes = count;
+                resultats.Add(resultat);
+            }
+            
+            return resultats;
         }
     }
 }
--- ChoixResto/Models/Interface1.cs
+++ ChoixResto/Models/Interface1.cs
@@ -10,7 +10,17 @@ namespace ChoixResto.Models
     {
         void CreerRestaurant(string nom, string telephone);
         void ModifierRestaurant(int id, string nom, string telephone);
+        bool RestaurantExiste(string nom);
+        Utilisateur ObtenirUtilisateur(int id);
+        Utilisateur ObtenirUtilisateur(string id);
         List<Resto> ObtientTousLesRestaurants();
+        int AjouterUtilisateur(string v1, string v2);
+        Utilisateur Authentifier(string v1, string v2);
+        bool ADejaVote(int idSondage, string idStr);
+        int CreerUnSondage();
+        void AjouterVote(int idSondage, int idResto, int idUtilisateur);
+        List<Resultats> ObtenirLesResultats(int idSondage);
+
     }
 
 
